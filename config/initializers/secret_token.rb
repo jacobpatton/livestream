ec319efd71c445cc1233d94dbcb91f34adc1936e
Jacob Patton <jacob@trisignia.com>
2013-05-21 16:16:06 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Livestream::Application.config.secret_key_base = 'c8e417bb2ffa9bebaccd28ccd10173dd989ce03b1efe0da0ef418168cd7f47d2f184836e5a367968df4e24fc746acc9b55ecc35cb0a1ffa57281989211236367'
